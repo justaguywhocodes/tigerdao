7087846475e735b38f4d52f41d1763ad617e9757
Derek Martin <derekmartinsf@gmail.com>
2022-01-29 13:22:36 -0800

    add nft minting for members and page

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,25 @@
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+const bundleDrop = sdk.getBundleDropModule(
+    process.env.BUNDLE_DROP_ADDRESS
+);
+
+(async () => {
+    try {
+        const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+
+        // Specify the conditions. 
+        claimConditionFactory.newClaimPhase({
+            startTime: new Date(),
+            maxQuantity: 50_000,
+            maxQuantityPerTransaction: 1
+        });
+
+        await bundleDrop.setClaimCondition(0, claimConditionFactory);
+        console.log("🐅 C'mon bro 😎 I got tigerBlood,#WINNING - Successfully set the claim condition on bundle drop:", bundleDrop.address);
+
+    } catch (err) {
+        console.error("Failed to set claim condition", err);
+    }
+})();
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
 import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
+const sdk = new ThirdwebSDK("rinkeby");
+
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x7Cf2722391982CA4AD1066Ffef50e76FB7a10e2D"
+);
 
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
+
+  const signer = provider ? provider.getSigner() : undefined;
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  const [isClaiming, setIsClaiming] = useState(false);
+
   console.log(`👋🏽 address: ${address}`);
+
+  useEffect(() => {
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
+
+  useEffect(() => {
+    if (!address) {
+
+      return;
+    }
+    
+    // Check if the user has the NFT by enabling bundleDropModule.balanceOf
+    return bundleDropModule
+      .balanceOf(address, "0")
+      .then((balance) => {
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log('#WINNING 🎉 - user has a membership NFT ');
+        } else {
+          setHasClaimedNFT(false);
+          console.log("#WEAK - 😭 this user doesn't have a membership NFT.")
+
+        }
+      })
+      .catch((err) => {
+        setHasClaimedNFT(false);
+        console.log('WEAK - 😭 failed to pull nft balance', err);
+      })
+
+  }, [address]);
+
 
   if (!address) {
     return (
@@ -16,10 +60,44 @@ const App = () => {
     );
   }
 
-  return (
-    <div className="landing">
-      <h1>👀 wallet connected, I call that #WINNING</h1>
-      <iframe style={{margin: 'auto'}} src="https://giphy.com/embed/x0kMYoT7J31i8" width="480" height="410" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-winning-charlie-sheen-x0kMYoT7J31i8">via GIPHY</a></p>
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🐅 tigerDAO Members Page</h1>
+        <p>Congratulations on being a member bro, that's some #epicwinning right there.</p>
+      </div>
+    )
+  }
+
+  const mintNFT = () => {
+    setIsClaiming(true);
+
+    bundleDropModule
+      .claim("0", 1)
+      .then(() => {
+        setHasClaimedNFT(true);
+        console.log("#EPICWINNING 🎉 - user has a membership NFT");
+        console.log("🌊 Successfully minted NFT, congrats you now have tigerblood");
+        console.log(`Check it here: https://testnets.opensea.io/assets/${bundleDropModule.address.toLowerCase()}/0`);
+
+      })
+      .catch((err) => {
+        console.error("#WEAK - failed to claim", err);
+
+      })
+      .finally(() => {
+        setIsClaiming(false);
+      });
+  };
+
+  return (  
+    <div className="mint-nft">
+      <h1>Mint your free 🐅 tigerDAO Membership NFT</h1>
+      <button disabled={isClaiming} 
+      onClick={() => mintNFT()}>
+        {isClaiming ? "Minting..." : "Mint your tigerBLOOD (NFT) - its free bro"  }
+
+      </button>
     </div>
   );
 };
